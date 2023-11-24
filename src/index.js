5e87b2d2af62fc62326714d0782d3a9bb555c228
olewandowski1 <olewandowski@soldevelo.com>
2023-11-24 11:52:49 +0100

    OM-78: sonar, prettier, eslint, template adjustments

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import messages_en from "./translations/en.json";
 
 const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
-}
+};
 
-export const TemplateModule = (cfg) => {
+export const WorkerVoucherModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
-}
+};
