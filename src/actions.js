d9bf9bb63ba79339b664e6cd7f8dbe0c1ca1a1a2
olewandowski1 <olewandowski@soldevelo.com>
2023-12-11 12:57:05 +0100

    OM-92: handle acquirement for generic voucher

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,13 @@
 import {
   formatPageQueryWithCount,
   graphql,
+  formatMutation,
+  graphqlWithVariables,
 } from '@openimis/fe-core';
 import { ACTION_TYPE } from './reducer';
-import { CLEAR } from './utils/action-type';
+import {
+  CLEAR, ERROR, REQUEST, SUCCESS,
+} from './utils/action-type';
 
 const WORKER_VOUCHER_PROJECTION = (modulesManager) => [
   'id',
@@ -33,3 +37,41 @@ export const clearWorkerVoucher = () => (dispatch) => {
     type: CLEAR(ACTION_TYPE.GET_WORKER_VOUCHER),
   });
 };
+
+export function genericVoucherValidation(phCode, quantity) {
+  return graphqlWithVariables(
+    `
+    query genericVoucherValidation($phCode: ID!, $quantity: Int) {
+      acquireUnassignedValidation (
+        economicUnitCode: $phCode,
+        count: $quantity,
+      ) {
+        count,
+        price,
+        pricePerVoucher
+      }
+    }
+    `,
+    { phCode, quantity },
+  );
+}
+
+export function acquireGenericVoucher(phCode, quantity, clientMutationLabel) {
+  const mutationInput = `
+  ${phCode ? `economicUnitCode: "${phCode}"` : ''}
+  ${quantity ? `count: ${quantity}` : ''}
+  `;
+  const mutation = formatMutation('acquireUnassignedVouchers', mutationInput, clientMutationLabel);
+  const requestedDateTime = new Date();
+
+  return graphql(
+    mutation.payload,
+    [REQUEST(ACTION_TYPE.MUTATION), SUCCESS(ACTION_TYPE.ACQUIRE_GENERIC_VOUCHER), ERROR(ACTION_TYPE.MUTATION)],
+    {
+      actionType: ACTION_TYPE.ACQUIRE_GENERIC_VOUCHER,
+      clientMutationId: mutation.clientMutationId,
+      clientMutationLabel,
+      requestedDateTime,
+    },
+  );
+}
