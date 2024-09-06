3c172de932d65e3d5a87a058e88d383de8634958
Oliver Lewandowski <109145288+olewandowski1@users.noreply.github.com>
2024-09-06 10:09:35 +0200

    OM-289: send a POST redirect when accessing mpay, add redirects (#63)
    * OM-287: block  acquirement and assignment dialog if action is performed
    
    * OM-289: send a POST redirect when accessing mpay, add redirects

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,12 +3,42 @@
 import { baseApiUrl } from '@openimis/fe-core';
 import { MPAY_BILL_URL } from '../constants';
 
+const fetchMPayArgs = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+};
+
+const submitMPayPOST = async (url, args) => {
+  const form = document.createElement('form');
+  form.method = 'POST';
+  form.action = url;
+  form.target = '_blank';
+
+  Object.entries(args).forEach(([key, value]) => {
+    const input = document.createElement('input');
+
+    input.type = 'hidden';
+    input.name = key;
+    input.value = value;
+
+    form.appendChild(input);
+  });
+
+  document.body.appendChild(form);
+
+  form.submit();
+};
+
 export const payWithMPay = async (billId) => {
   try {
     const redirectToURL = new URL(`${window.location.origin}${baseApiUrl}${MPAY_BILL_URL}`);
     redirectToURL.searchParams.set('bill', billId);
 
-    window.location.href = redirectToURL.href;
+    const { url, args } = await fetchMPayArgs(redirectToURL.href);
+
+    await submitMPayPOST(url, args);
   } catch (error) {
     throw new Error('Redirection to MPay failed.', error);
   }
